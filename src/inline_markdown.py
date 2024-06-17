24bb8fde20f30990eb1a9dcee8ad71596f17b98c
Swapnilgupta8585 <swapnilgupta19812004@gmail.com>
2024-06-17 10:34:54 +0530

    add text_to_textnode function

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,6 +1,26 @@
 from textnode import TextNode
 import re
 
+
+def text_to_textnodes(text):
+    original_node = TextNode(text,'text')
+    
+    split_for_bold = split_nodes_delimiter([original_node],"**","bold")
+    
+    split_for_italic = split_nodes_delimiter(split_for_bold,"*","italic")
+    
+    split_for_code = split_nodes_delimiter(split_for_italic,"`","code")
+    
+    split_for_images = split_nodes_images(split_for_code)
+    
+    split_for_links = split_nodes_links(split_for_images)
+    
+    return split_for_links
+
+
+
+
+
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
         new_nodes = []
         for node in old_nodes:
@@ -53,29 +73,40 @@ def split_nodes_images(old_nodes):
             split_node.append(TextNode(image_tup[0],"image",image_tup[1]))
             node_text = split_wrt_images[1]
         new_node.extend(split_node)
+        if node_text != "":
+            new_node.append(TextNode(node_text,"text"))
+        
     
     return new_node
 
 
 def split_nodes_links(old_nodes):
     new_node  = []
+    
     for node in old_nodes:
+        
         split_node = []
         link_tuples = extract_markdown_links(node.text)
+       
         
         if len(link_tuples) == 0:
             split_node.append(node)
             new_node.extend(split_node)
             continue
         node_text = node.text
+        
         for link_tup in link_tuples:
             split_wrt_links = node_text.split(f"[{link_tup[0]}]({link_tup[1]})",1)
             
             if split_wrt_links[0] != "":
                 split_node.append(TextNode(split_wrt_links[0],"text"))
+                
             split_node.append(TextNode(link_tup[0],"link",link_tup[1]))
             node_text = split_wrt_links[1]
         new_node.extend(split_node)
+        if node_text != "":
+            new_node.append(TextNode(node_text,"text"))
+        
    
     return new_node
 
