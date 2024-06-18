0ef66f2c92099a80eacfde380b37e1dbab2deb8d
Swapnilgupta8585 <swapnilgupta19812004@gmail.com>
2024-06-18 09:39:55 +0530

    add block_to_block_type func

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,13 +1,69 @@
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_unordered_list = "unordered_list"
+block_type_ordered_list = "ordered_list"
+
+
 def markdown_to_blocks(raw_markdown):
     if raw_markdown == "":
         return []
     list_blocks = []
-    split_blocks = raw_markdown.split('\n\n')
+    split_blocks = raw_markdown.split("\n\n")
     for block in split_blocks:
         if block == "":
             continue
         block = block.strip()
         list_blocks.append(block)
-    
+
     return list_blocks
+
+
+def block_to_block_type(block):
+    lines = block.split("\n")
+    if (
+        block.startswith("# ")
+        or block.startswith("## ")
+        or block.startswith("### ")
+        or block.startswith("#### ")
+        or block.startswith("##### ")
+        or block.startswith("###### ")
+    ):
+        return block_type_heading
+
+    if len(lines) > 0 and lines[0].startswith("```") and lines[-1].startswith("```"):
+        return block_type_code
+
+    if block.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return block_type_paragraph
+        return block_type_quote
+    
+    if block.startswith("* "):
+        for line in lines:
+            if not line.startswith("* "):
+                return block_type_paragraph
+        return block_type_unordered_list
+    
+    if block.startswith("- "):
+        for line in lines:
+            if not line.startswith("- "):
+                return block_type_paragraph
+        return block_type_unordered_list
+    
+    
+    if block.startswith("1. "):
+        count = 1
+        for line in lines:
+            if not line.startswith(f"{count}. "):
+                return block_type_paragraph
+            count += 1
+        return block_type_ordered_list
+    
+    return block_type_paragraph
+            
         
+            
+         
