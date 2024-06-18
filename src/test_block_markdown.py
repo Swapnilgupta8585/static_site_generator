434ab68557f53c7660045c86a59f4a2f28826df6
Swapnilgupta8585 <swapnilgupta19812004@gmail.com>
2024-06-18 09:40:39 +0530

    add unit tests for block_to_block_type func

--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,6 +1,15 @@
 import unittest
 
-from block_markdown import markdown_to_blocks
+from block_markdown import (
+    markdown_to_blocks,
+    block_to_block_type,
+    block_type_paragraph,
+    block_type_heading,
+    block_type_code, 
+    block_type_quote,
+    block_type_unordered_list,
+    block_type_ordered_list 
+                            )                         
 
 class Test_block_markdown(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -45,6 +54,54 @@ This is the same paragraph on a new line
                 "* This is a list\n* with items",
             ],
         )
+
+
+
+
+
+
+
+    def test_block_to_block_type(self):
+        md = """
+This is **bolded** paragraph
+
+
+
+
+This is another paragraph with *italic* text and `code` here
+This is the same paragraph on a new line
+
+* This is a list
+* with items
+"""
+        blocks = markdown_to_blocks(md)
+        block_type = []
+        for block in blocks:
+            block_type.append(block_to_block_type(block))
+        
+        self.assertListEqual([
+            block_type_paragraph,
+            block_type_paragraph,
+            block_type_unordered_list
+        ],block_type)
+
+        block = "# heading"
+        self.assertEqual(block_to_block_type(block), block_type_heading)
+        block = "```\ncode\n```"
+        self.assertEqual(block_to_block_type(block), block_type_code)
+        block = "> quote\n> more quote"
+        self.assertEqual(block_to_block_type(block), block_type_quote)
+        block = "* list\n* items"
+        self.assertEqual(block_to_block_type(block), block_type_unordered_list)
+        block = "1. list\n2. items"
+        self.assertEqual(block_to_block_type(block), block_type_ordered_list)
+        block = "paragraph"
+        self.assertEqual(block_to_block_type(block), block_type_paragraph)
+
+        
+
+    
+
 
 if __name__ == "__main__":
     unittest.main()
